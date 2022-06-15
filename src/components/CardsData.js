23e7ba4028d17ba4b9c941905206f2609d5fc3d0
Aidana555 <aidananiazalieva27@gmail.com>
2022-06-15 16:50:10 +0600

    gffbgvb

--- src/components/CardsData.js
+++ src/components/CardsData.js
@@ -184,101 +184,11 @@ const Cardsdata = [
           "The Shawshank Redemption is a 1994 American drama film written and directed by Frank Darabont, based on the 1982 Stephen King novella Rita Hayworth and Shawshank Redemption.",
         category: "horror literature",
         image: "./img/shawshank .jpg",
+
+  
       },
-        rname: "Лимонад Simple banana",
-        imgdata: "./img/banan.jpg",
-        address: "Frash ",
-        price: '200',
-        rating: "3.8",
-        arrimg: "https://www.complexbar.ru/recipe/limonady",
-        qnty:0
-    },
-    {
-        id: 2,
-        rname: "Тропический клауди ",
-        imgdata: "./img/cloudy.jpg",
-        address: "Frash ",
-       
-        price: 200,
-        rating: "3.9",
-        arrimg: "https://www.complexbar.ru/recipe/limonady",
-         qnty:0
-    },
-    {
-        id: 3,
-        rname: "Клубнично-грейпфрутовый лимонад",
-        imgdata: "./img/430a811c3c1a697f0154c519ff1ca844.jpg",
-        address: "Frash ",
-        price: 70,
-        rating: "4.2",
-        arrimg: "https://www.complexbar.ru/recipe/limonady",        
-         qnty:0
-    },
-    {
-        id: 4,
-        rname: "Лимонад с барбадосской вишней и манго",
-        imgdata: "./img/cherry.jpg",
-        address: "Frash ",
-        price: 70,
-        rating: "3.8",
-        arrimg: "https://www.complexbar.ru/recipe/limonady",         
-        qnty:0
-    },
-    {
-        id: 5,
-        rname: "Зелёный безалкогольный физз ",
-        imgdata: "./img/green.jpg",
-        address: "Frash ",
-        price: 210,
-        rating: "4.0",
-        arrimg: "https://www.complexbar.ru/recipe/limonady",
-         qnty:0
-    },
-    {
-        id: 6,
-        rname: "Anjoy Latenight Meals",
-        imgdata: "./img/0d60505c14a30a58c6105f719b9d5579.jpg",
-        address: "Wraps FastFood, Chines",
-      
-        price: 200,
-        rating: "3.8",
-        arrimg: "https://www.complexbar.ru/recipe/limonadyp",
-         qnty:0
-    },
-    {
-        id: 7,
-        rname: "Hocco Eatery",
-        imgdata: "./img/gum.jpg",
-        address: "Frash ",
-        price: 300,
-        rating: "3.8",
-        arrimg: "https://www.complexbar.ru/recipe/limonady",
-         qnty:0
-    },
-    {
-        id: 8,
-        rname: "Chai Wai",
-        imgdata: "https://www.complexbar.ru/recipe/limonady",
-        address: "Tea, Coffee, Shake, Beverages",
-        delimg: "https://www.complexbar.ru/recipe/limonady",
-        somedata: "500 + order placed from here recently",
-        price: 100,
-        rating: "3.2",
-        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
-         qnty:0
-    },
-    {
-        id: 9,
-        rname: "HL Frankie",
-        imgdata: "./img/430a811c3c1a697f0154c519ff1ca844.jpg",
-        address: "Burger, Sandwich, Fast Food",
-        delimg: "https://www.complexbar.ru/recipe/limonady",
-        somedata: "2525 + order placed from here recently",
-        price: 100,
-        rating: "3.8",
-        arrimg: "https://www.complexbar.ru/recipe/limonady",
-         qnty:0
-    },
+  
+ 
 ];
 
 export default Cardsdata;
